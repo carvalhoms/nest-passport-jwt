40cefc5d413daa2fcea338b4d30e1a83a330f110
Marcelo Carvalho <carvalhoms@icloud.com>
2022-09-21 23:09:29 -0300

    CRUD User Service

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FindOneOptions, Repository } from 'typeorm';
+import { UsersEntity } from './entities/users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UsersEntity)
+    private readonly usersRepository: Repository<UsersEntity>,
+  ) {}
+
+  async findAll() {
+    return this.usersRepository.find({
+      select: ['id', 'firstName', 'lastName'],
+    });
+  }
+
+  async findOneOrFail(options?: FindOneOptions<UsersEntity>) {
+    try {
+      return this.usersRepository.findOneOrFail(options);
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
+  }
+
+  async update(id: string, data: any) {
+    const user = await this.findOneOrFail({ where: { id } });
+    this.usersRepository.merge(user, data);
+    return await this.usersRepository.save(user);
+  }
+
+  async destroy(id: string) {
+    await this.findOneOrFail({ where: { id } });
+    return this.usersRepository.softDelete({ id });
+  }
+}
