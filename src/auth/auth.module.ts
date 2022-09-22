bf704388af13f70098bbd5e8819b1f6b3e954fce
Marcelo Carvalho <carvalhoms@icloud.com>
2022-09-22 15:29:45 -0300

    Import and config JwtModule in auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/app/users/users.module';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, PassportModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      privateKey: '6ycNSv97/x2i7NjqBNVXlo6gbBW8in9urdFueG6AQ8k=', // Comend generated terminal: #openssl rand -base64 32
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService],
 })
 export class AuthModule {}
