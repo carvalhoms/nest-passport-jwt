823c44808b546715346f33e0f9e577f8a418fa0e
Marcelo Carvalho <carvalhoms@icloud.com>
2022-09-22 13:19:32 -0300

    Install @types/bcrypt + error entity app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { UsersModule } from './app/users/users.module';
       username: process.env.TYPEORM_USERNAME,
       password: process.env.TYPEORM_PASSWORD,
       database: process.env.TYPEORM_DATABASE,
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
       synchronize: true,
     } as TypeOrmModuleOptions),
     UsersModule,
