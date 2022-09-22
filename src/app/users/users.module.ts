77aa65e41ec5c8b003ecbf1797be5115270c59b3
Marcelo Carvalho <carvalhoms@icloud.com>
2022-09-22 13:10:57 -0300

    Import TypeOrmModule users.module

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersEntity } from './entities/users.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UsersEntity])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
