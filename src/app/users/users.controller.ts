d61b57198514c79630ace63728d66e6df855998d
Marcelo Carvalho <carvalhoms@icloud.com>
2022-09-21 21:15:22 -0300

    Routes created users.controller

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async index() {
+    return null;
+  }
+
+  @Post()
+  async store() {
+    return null;
+  }
+
+  @Get(':id')
+  async show() {
+    return null;
+  }
+
+  @Put(':id')
+  async update() {
+    return null;
+  }
+
+  @Delete(':id')
+  async destroy() {
+    return null;
+  }
+}
