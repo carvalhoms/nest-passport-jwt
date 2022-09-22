5ce752a57fa67fd8ff1dae89dcac6a0f6c2921cf
Marcelo Carvalho <carvalhoms@icloud.com>
2022-09-21 21:30:38 -0300

    Created users.entity

---
+++ src/app/users/entities/users.entity.ts
@@ -0,0 +1,33 @@
+import {
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+export class UsersEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'first_name' })
+  firstName: string;
+
+  @Column({ name: 'last_name' })
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
+}
