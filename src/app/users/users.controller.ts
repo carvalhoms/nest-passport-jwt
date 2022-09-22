acf75b87a27767db044fa31c6266032bf87e2294
Marcelo Carvalho <carvalhoms@icloud.com>
2022-09-22 13:01:05 -0300

    Controller Users Routes

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -7,26 +20,30 @@ export class UsersController {
 
   @Get()
   async index() {
-    return null;
+    return await this.usersService.findAll();
   }
 
   @Post()
-  async store() {
-    return null;
+  async store(@Body() body: CreateUserDto) {
+    return await this.usersService.store(body);
   }
 
   @Get(':id')
-  async show() {
-    return null;
+  async show(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.usersService.findOneOrFail({ where: { id } });
   }
 
   @Put(':id')
-  async update() {
-    return null;
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() body: UpdateUserDto,
+  ) {
+    return await this.usersService.update(id, body);
   }
 
   @Delete(':id')
-  async destroy() {
-    return null;
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.usersService.destroy(id);
   }
 }
