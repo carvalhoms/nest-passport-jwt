2409ee2bd54aeee6480d03a2a5acc2328129e008
Marcelo Carvalho <carvalhoms@icloud.com>
2022-09-22 15:25:40 -0300

    Imports auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/app/users/users.module';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService]
+  imports: [ConfigModule.forRoot(), UsersModule, PassportModule],
+  providers: [AuthService],
 })
 export class AuthModule {}
